741403a92e09a5e6a0d6efa8dc47b9c0058ede41
Sergey Malinin <smalinin@openlinksw.com>
2023-04-26 00:23:15 +0300

    Update code

--- src/test.js
+++ src/test.js
@@ -27,10 +27,39 @@ function initButton(id, action) {
   document.getElementById(id).addEventListener('click', action)
 }
 
+
+function checkSession() {
+  const session = client.getDefaultSession();
+  console.log('handleRedirectAfterLogin(): session:', session);
+  console.log('handleRedirectAfterLogin(): session.info.isLoggedIn: ', session.info.isLoggedIn);
+  if (session.info.isLoggedIn) {
+    console.log('handleRedirectAfterLogin(): session.info.webId: ', session.info.webId);
+    // Update the page with the login status.
+    if (session.info && session.info.isLoggedIn && session.info.webId) {
+      show('logged')
+      show('logout')
+      setField("webid", session.info.webId)
+    }
+
+  }
+}
+
+
+
 function initButtons () {
 //  initButton('login_opl_uriburner', () => login('https://linkeddata.uriburner.com'))
   initButton('login_opl_uriburner', () => login('https://solid.openlinksw.com:8446'))
   initButton('logout', () => logout())
+
+  initButton('test_restore', () => {
+    var u = localStorage.getItem('myCode');
+    if (u) {
+      await client.handleIncomingRedirect({url:u, restorePreviousSession: true});
+      checkSession();
+    }
+    
+  })
+
 }
 
 function show (id) {
@@ -47,6 +76,21 @@ function setField (id, value) {
   document.addEventListener('DOMContentLoaded', async () => {
     initButtons();
   })
+
+  const authCode =
+    new URL(window.location.href).searchParams.get("code") ||
+    // FIXME: Temporarily handle both auth code and implicit flow.
+    // Should be either removed or refactored.
+    new URL(window.location.href).searchParams.get("access_token");
+
+  if (authCode) {
+    localStorage.setItem('myCode', location.href);
+  }
+
+
+
+
+
 
 
 
